2b3a30ff13fdb516873dad958921b0c08178c1e9
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-10-01 09:54:20 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -2,6 +2,8 @@ package com.campusdual.classroom;
 
 public class Exercise28 {
     public static void main(String[] args) {
-
+        int dividend = 73, divisor = 0;
+        int result = dividend / divisor;
+        System.out.println("El resultado de la división es: " + result);
     }
 }
